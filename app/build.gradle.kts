8b0c6ba8584b7f547f6b04831bdb72a7e8bc8a48
Nakul Upasani <nakul.upasani85@gmail.com>
2025-11-19 21:43:46 -0500

    Updated Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,17 @@ android {
 }
 
 dependencies {
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    // View Model
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")
+    // Local SQLite
+    implementation("androidx.datastore:datastore-preferences:1.1.7")
+    // API
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation("io.coil-kt:coil-compose:2.7.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
