65a721e182666f58c5b63f5d7222db502023fa00
Stefan <103537403+Stefan-Kob@users.noreply.github.com>
2025-11-20 15:29:06 -0500

    Added splash Screen

--- app/src/main/java/com/example/mobile_final_dealza/features/SplashScreen.kt
+++ app/src/main/java/com/example/mobile_final_dealza/features/SplashScreen.kt
@@ -1,10 +1,35 @@
 package com.example.mobile_final_dealza.features
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.example.mobile_final_dealza.R
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen(
-
-){
-
+fun SplashScreen(onTimeout: () -> Unit) {
+    LaunchedEffect(true) {
+        delay(2000) // Wait 2 sec
+        onTimeout()
+    }
+    Box (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(painterResource(R.drawable.logo), contentDescription = "Logo")
+            Text( text = stringResource(id = R.string.app_name), color = MaterialTheme.colorScheme.onBackground, style = MaterialTheme.typography.headlineMedium) }
+    }
 }
--- app/src/main/java/com/example/mobile_final_dealza/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/mobile_final_dealza/navigation/AppNavHost.kt
@@ -3,7 +3,10 @@ package com.example.mobile_final_dealza.navigation
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.mobile_final_dealza.features.HomeScreen
+import com.example.mobile_final_dealza.features.SplashScreen
 import com.example.mobile_final_dealza.viewmodel.DealViewModel
 import com.example.mobile_final_dealza.viewmodel.UserViewModel
 
@@ -14,5 +17,21 @@ fun DealNavHost(navController: androidx.navigation.NavHostController = rememberN
 
     // NavHost for routing
     NavHost(navController = navController, startDestination = "list", modifier = modifier) {
+
+        // Splash Screen
+        composable(route = "splash_screen") {
+            SplashScreen(onTimeout = {
+                // Navigate to the login screen after the splash delay
+                navController.navigate(route = "home_screen") {
+                    popUpTo(route = "splash_screen") { inclusive = true }
+                }
+            })
+        }
+
+        composable(route = "home_screen") {
+            HomeScreen()
+        }
+
+
     }
 }
